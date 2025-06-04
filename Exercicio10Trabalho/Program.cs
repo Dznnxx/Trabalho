34fed55ea2cfcdca3a7b5592f40240fa4c6f14b1
Deidson <limagalvaod@gmail.com>
2025-06-04 19:34:40 -0300

    arquivos importados 2

---
+++ Exercicio10Trabalho/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio10Trabalho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
