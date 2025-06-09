1d9e665a3ac7ddf3da9f27345539ecf916edba0e
Dznnxx <limagalvaod@gmail.com>
2025-06-09 19:21:02 -0300

    Update Program.cs

--- Exercicio10Trabalho/Program.cs
+++ Exercicio10Trabalho/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Exercicio10Trabalho
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿int x1, x2, y1, y2, deltaX, distancia;
+
+Console.WriteLine("Informe x1:");
+x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o y1: ");
+y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o x2:");
+x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o y2: ");
+y2 = Convert.ToInt32(Console.ReadLine());
